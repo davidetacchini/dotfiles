c16b6ece34bb07da55778e3ee52fa1a79a283b04
Davide <84925446+davidetacchini@users.noreply.github.com>
2025-06-11 11:46:11 +0200

    Added nvim-highlight-colors plugin

--- .config/nvim/lua/davide/plugins/nvim-cmp.lua
+++ .config/nvim/lua/davide/plugins/nvim-cmp.lua
@@ -44,10 +44,18 @@ return {
         { name = "path" },
       }),
       formatting = {
-        format = lspkind.cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
+        format = function(entry, item)
+          local color_item = require("nvim-highlight-colors").format(entry, { kind = item.kind })
+          item = require("lspkind").cmp_format({
+            maxwidth = 50,
+            ellipsis_char = "...",
+          })(entry, item)
+          if color_item.abbr_hl_group then
+            item.kind_hl_group = color_item.abbr_hl_group
+            item.kind = color_item.abbr
+          end
+          return item
+        end,
       },
     })
   end,
