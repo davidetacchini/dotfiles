0133cec770e8baf8479a6e9526bb266dc6a7c85b
Davide <84925446+davidetacchini@users.noreply.github.com>
2025-09-04 11:30:44 +0200

    feat: add noice.nvim for enhanced UI messaging

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    presets = {
+      bottom_search = true, -- use a classic bottom cmdline for search
+      command_palette = false, -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+    },
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    -- "rcarriga/nvim-notify",
+  },
+}
