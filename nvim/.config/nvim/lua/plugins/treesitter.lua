07890dc5dea7212f8119b380ec8f50912454e6da
Smyile <84925446+davidetacchini@users.noreply.github.com>
2026-03-31 01:15:40 +0200

    refactor: migrate treesitter to main branch API
    Use nvim-treesitter main branch with explicit parser install
    and FileType autocmd instead of legacy configs module.
    Separate nvim-ts-autotag into its own plugin spec.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,47 +1,61 @@
+local parsers = {
+  "c",
+  "cpp",
+  "python",
+  "javascript",
+  "typescript",
+  "html",
+  "css",
+  "htmldjango",
+  "tsx",
+  "lua",
+  "json",
+  "yaml",
+  "vue",
+  "tmux",
+  "sql",
+  "bash",
+  "http",
+  "dockerfile",
+  "markdown",
+  "markdown_inline",
+  "vimdoc",
+}
+
+local function parser_filetypes()
+  local fts = {}
+  local seen = {}
+  for _, parser in ipairs(parsers) do
+    for _, ft in ipairs(vim.treesitter.language.get_filetypes(parser)) do
+      if not seen[ft] then
+        seen[ft] = true
+        fts[#fts + 1] = ft
+      end
+    end
+  end
+  return fts
+end
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = {
-    {
-      "windwp/nvim-ts-autotag",
-      opts = {},
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install(parsers)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = parser_filetypes(),
+        callback = function()
+          vim.treesitter.start()
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "cpp",
-        "python",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "htmldjango",
-        "tsx",
-        "lua",
-        "json",
-        "yaml",
-        "vue",
-        "tmux",
-        "sql",
-        "bash",
-        "http",
-        "dockerfile",
-        "markdown",
-        "markdown_inline",
-        "vimdoc",
-        -- "gdscript",
-        -- "gdresource",
-      },
-      sync_install = false,
-      auto_install = true,
-      ignore_install = {},
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end,
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+  },
 }
